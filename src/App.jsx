5354e2cfec6e475ab022d7d4df9b62f2eb989a8d
ZirgomHaidar <zirgomhaidar17@gmail.com>
2024-05-31 12:02:53 +0530

    DX: Add Footer to the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import Footer from "./components/Footer/Footer";
 import Home from "./components/Home/Home";
 
 function App() {
   return (
     <div>
       <Home />
+      <Footer />
     </div>
   );
 }
---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,41 @@
+import React from "react";
+import "./Footer.css";
+import heart from "../../assets/heart.svg";
+
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <div className="middle">
+          <div className="documentation-info">
+            <h2>Documentation</h2>
+            <h4>Maintainership form</h4>
+            <h4>GitHub</h4>
+            <h4>XDA</h4>
+            <h4>Device requirements</h4>
+          </div>
+          <div className="help-info">
+            <h2>Help & Support</h2>
+            <h4>Website</h4>
+            <h4>Telegram</h4>
+            <h4>Twitter</h4>
+            <h4>News</h4>
+          </div>
+          <div className="legal-info">
+            <h2>Legal Info</h2>
+            <h4>Privacy Policies</h4>
+            <h4>Disclaimer</h4>
+          </div>
+        </div>
+        <div className="bottom">
+          <p>
+            Made with <img src={heart} alt="heart where bro ?" />
+            by Zirgom Haidar
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Footer;
