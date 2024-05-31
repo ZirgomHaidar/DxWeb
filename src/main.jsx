514cdfb4ce4c1ef1053d8e2916dedc3e6de2837b
ZirgomHaidar <zirgomhaidar17@gmail.com>
2024-05-31 12:05:39 +0530

    DX: Add Product Sans Font and Apply it everywhere

--- src/main.jsx
+++ src/main.jsx
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
+import "./fonts/style.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
