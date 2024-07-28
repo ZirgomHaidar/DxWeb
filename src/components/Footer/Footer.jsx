3596a9bc45d89efebc8d50eb121c52903561a69c
ZirgomHaidar <zirgomhaidar17@gmail.com>
2024-07-28 19:35:26 +0530

    DX: Redesign Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,41 +1,65 @@
 import React from "react";
-import "./Footer.css";
 import heart from "../../assets/heart.svg";
+import logo from "../../assets/dxlogo_2.svg";
+import tg from "../../assets/footer_tg.svg";
+import gh from "../../assets/footer_gh.svg";
+import mf from "../../assets/footer_mf.svg";
+import x from "../../assets/footer_x.svg";
+import xda from "../../assets/footer_xda.svg";
+import donate from "../../assets/footer_donate.svg";
 
 const Footer = () => {
   return (
-    <div className="footer-container relative flex flex-col items-center justify-between h-[250px] md:h-[350px] bg-zinc-950">
-      <div className="middle flex md:gap-48 mt-8 md:mt-16">
-        <div className="documentation-info hidden md:block">
-          <h2>Documentation</h2>
-          <h4>Maintainership form</h4>
-          <h4>GitHub</h4>
-          <h4>XDA</h4>
-          <h4>Device requirements</h4>
-        </div>
-        <div className="help-info hidden md:block">
-          <h2>Help & Support</h2>
-          <h4>Website</h4>
-          <h4>Telegram</h4>
-          <h4>Twitter</h4>
-          <h4>News</h4>
-        </div>
-        <div className="legal-info">
-          <h2>Legal Info</h2>
-          <h4>Privacy Policies</h4>
-          <h4>Disclaimer</h4>
-        </div>
+    <div className="footer-container relative bottom-0 flex flex-col justify-between h-[250px] md:h-[220px] bg-zinc-950 pt-6 pb-5 px-8 rounded-t-[20px]">
+      <div className="dx flex flex-row items-center gap-3">
+        <img className="w-20" src={logo} alt="" />
+        <h1
+          className="text-[#3282B8] text-4xl"
+          style={{ fontFamily: "Product Sans Bold" }}
+        >
+          DroidX-UI
+        </h1>
       </div>
-      <div className="bottom relative bottom-0">
-        <p className="flex items-center">
-          Made with{" "}
-          <img
-            className="p-2 hover:scale-125 duration-300 ease-in-out"
-            src={heart}
-            alt="heart where bro ?"
-          />
-          by Zirgom Haidar & Kshitij Bhale
-        </p>
+      <div className="legal-info flex flex-row text-xl gap-7">
+        <h4>Privacy Policy</h4>
+        <h4>Disclaimer</h4>
+      </div>
+      <div className="links flex gap-4 h-[50px]">
+        <a
+          href="https://t.me/DroidXUI_chats"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={tg} alt="tg" />
+        </a>
+        <a
+          href="https://github.com/DroidX-UI"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={gh} alt="tg" />
+        </a>
+        <a
+          href="https://twitter.com/DroidX_UI"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={x} alt="X" />
+        </a>
+        <a
+          href="https://github.com/DroidX-UI-Devices/vendor_droidxOTA/blob/14/README.md"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={mf} alt="tg" />
+        </a>
+        <a
+          href="https://twitter.com/DroidX_UI"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src={donate} alt="X" />
+        </a>
       </div>
     </div>
   );
